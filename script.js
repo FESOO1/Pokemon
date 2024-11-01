023173e9ab246b1fb13ac0524d3b32598728a6d8
FES001 <firdavs7794948@gmail.com>
2024-11-02 01:56:47 +0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const outputContainer = document.querySelector('.output');
+let pokemonNumbers = 10;
+
+
+for (let i = 0; i < pokemonNumbers; i++) {
+    const outputItself = document.createElement('div');
+    outputItself.classList.add('');
+};
