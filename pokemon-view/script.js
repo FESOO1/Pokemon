d88b3852e4ea2db564139ef4427818eab893665a
FES001 <firdavs7794948@gmail.com>
2024-11-02 21:18:16 +0500

    working on it.

--- pokemon-view/script.js
+++ pokemon-view/script.js
@@ -1,7 +1,38 @@
-const pokemonDetails = document.querySelector('.pokemon-details');
+const pokemonDetailsInner = document.querySelector('.pokemon-details-inner');
+const pokemonPictureContainer = document.querySelector('.pokemon-details-picture');
 const pokemonDetailsPicture = document.querySelector('.pokemon-details-picture-itself');
 const pokemonDetailsName = document.querySelector('.pokemon-detail-name');
 const pokemonDetailsWeight = document.querySelector('.pokemon-detail-weight');
+let positionX = 0, positionY = 0;
+
+// ROTATING THE IMAGE BASED ON WHERE THE MOUSE CORDANATION IS.
+
+pokemonDetailsInner.addEventListener('mouseenter', e => {
+
+    pokemonDetailsInner.addEventListener('mousemove', mouseMove);
+
+    pokemonDetailsInner.addEventListener('mouseleave', () => {
+        pokemonDetailsInner.removeEventListener('mousemove', mouseMove);
+
+        pokemonPictureContainer.style.transition = 'transform 500ms';
+        pokemonPictureContainer.style.transform = 'rotateX(0deg) rotateY(0deg)';
+
+        setTimeout(() => {
+            pokemonPictureContainer.style.transition = 'none';
+        }, 501);
+    });
+});
+
+function mouseMove(e) {
+    positionX = e.clientX - 500;
+    positionY = e.clientY - 500;
+
+    console.log(positionX, positionY);
+
+    pokemonPictureContainer.style.transform = `perspective(500px) rotateY(${positionX}deg)`;
+};
+
+// ACCESSING THE DATA FROM LOCAL STORAGE TO DISPLAY IT IN THE CONTAINERS.
 
 function getDataFromLocalStorage() {
     const pokemonName = localStorage.getItem('pokemonName'), pokemonPicture = localStorage.getItem('pokemonPicture'), pokemonWeight = localStorage.getItem('pokemonWeight');
